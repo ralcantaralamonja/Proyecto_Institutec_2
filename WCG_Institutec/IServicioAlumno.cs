81fd07a5a503e555a7d27703cf7ffbcd560d096f
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2023-10-19 19:56:35 -0500

    SERVICIOS CRUD: ALUMNO

--- WCG_Institutec/IServicioAlumno.cs
+++ WCG_Institutec/IServicioAlumno.cs
@@ -23,7 +23,7 @@ namespace WCG_Institutec
 
         [OperationContract]
         AlumnoDC ConsultarAlumno(String strCodigo);
-
+        
         [OperationContract]
         List<AlumnoDC> ListarAlumno();
 
@@ -54,37 +54,28 @@ namespace WCG_Institutec
         public String ApeMat { get; set; }
 
         [DataMember]
-        public String TelAlu { get; set; }
+        public String Id_Ubi { get; set; }
 
         [DataMember]
-        public String CorAlu { get; set; }
-
-        [DataMember]
-        public bool Estado { get; set; }
-
-        [DataMember]
-        public String SexoAl { get; set; }
+        public String Sexoal { get; set; }
 
         [DataMember]
         public DateTime FecNac { get; set; }
 
         [DataMember]
-        public String CodCar { get; set; }
+        public String CorAlu { get; set; }
 
         [DataMember]
-        public String DescCar { get; set; }
-
-        [DataMember]
-        public DateTime Fec_Registro { get; set; }
+        public String TelAlu { get; set; }
 
         [DataMember]
         public String Usu_Registro { get; set; }
 
         [DataMember]
-        public DateTime Fec_Ult_Mod { get; set; }
+        public bool Estado { get; set; }
 
         [DataMember]
-        public String Usu_Ult_Mod { get; set; }
+        public String CodCar { get; set; }
 
         [DataMember]
         public byte[] Foto { get; set; }
@@ -92,19 +83,36 @@ namespace WCG_Institutec
         [DataMember]
         public String direccion { get; set; }
 
+
+
+
+
+
+
+
+
+        //[DataMember]
+        //public String DescCar { get; set; }
+        
         [DataMember]
-        public String departamento { get; set; }
+        public DateTime Fec_Registro { get; set; }
 
         [DataMember]
-        public String distrito { get; set; }
+        public DateTime Fec_Ult_Mod { get; set; }
 
-        [DataMember]
-        public String Id_Ubi { get; set; }
+        //[DataMember]
+        //public String Usu_Ult_Mod { get; set; }
 
-        [DataMember]
-        public String IdFacu { get; set; }
+        //[DataMember]
+        //public String departamento { get; set; }
 
-        [DataMember]
-        public String DescFacu { get; set; }
+        //[DataMember]
+        //public String distrito { get; set; }
+
+        //[DataMember]
+        //public String IdFacu { get; set; }
+
+        //[DataMember]
+        //public String DescFacu { get; set; }
     }
 }
